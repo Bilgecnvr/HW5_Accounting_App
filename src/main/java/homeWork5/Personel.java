b6b07722f28727d0227950b070ca7f4e63e872e9
Bilgecnvr <bilgegngrnn@gmail.com>
2025-09-28 14:40:53 +0300

    HW5 maaş hesaplama uygulaması tamamlandı.

---
+++ src/main/java/homeWork5/Personel.java
@@ -0,0 +1,11 @@
+package homeWork5;
+
+public class Personel {
+    String ad;
+    int calismaGunu;
+
+    public Personel(String isim, int gun) {
+        ad = isim;
+        calismaGunu = gun;
+    }
+}
